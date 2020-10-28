9125ece9008a1ceaf866ceb0942ba4e8dae687bb
Paul Morales <paulginga@hotmail.com>
2020-10-27 19:53:32 -0500

    added seeds.sql file to db folder

---
+++ banking-system/db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO bank (name, checking_balance, savings_balance) VALUES ('John Doe', 25000, 10000);
+
+INSERT INTO bank (name, checking_balance, savings_balance) VALUES ('Mary Smith', 5000, 1000);
+
+INSERT INTO bank (name, checking_balance, savings_balance) VALUES ('Aaron Wilson', 250, 88000);
